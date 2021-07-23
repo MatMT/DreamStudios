7f0196ca27352ac08faa27ab740a06c4a154f781
MartinTorres987 <86280538+MartinTorres987@users.noreply.github.com>
2021-07-22 21:54:34 -0600

    carousel

--- Dreams!_DS Oficial/Página Principal/archivos-js/settings.js
+++ Dreams!_DS Oficial/Página Principal/archivos-js/settings.js
@@ -1,25 +1,17 @@
-const buttomPrev = document.getElementById("buttom-prev");
-const buttomNext = document.getElementById("buttom-next");
-const track = document.getElementById("track");
-const slickList = document.getElementById("slick-list");
-const slick = document.querySelectorAll(".slick");
+const fila = document.querySelector(".contenedor-principal");
+const peliculas = document.querySelector(".artista");
 
-const slickWidth = slick[0].offsetWidth;
+const flechaIzquierda = document.getElementById("flechaIz");
+const flechaDerecha = document.getElementById("flechaDe");
 
-buttomPrev.onclick = () => Move(1);
-buttomNext.onclick = () => Move(2);
+// Event Listener para la flecha derecha
 
-function Move(value){
-    const trackWidth = track.offsetWidth;
-    const listWidth = slickList.offsetWidth;
+flechaDerecha.addEventListener("click", () => {
+    fila.scrollLeft += fila.offsetWidth;
+})
 
-    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+// Event Listener para la flecha Izquierda
 
-
-    if(leftPosition < (trackWidth - slickWidth) && value==2 ){
-        track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
-    } else if (leftPosition > 0 && value == 1) {
-        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
-    }
-
-}
+flechaIzquierda.addEventListener("click", () => {
+    fila.scrollLeft -= fila.offsetWidth;
+})
