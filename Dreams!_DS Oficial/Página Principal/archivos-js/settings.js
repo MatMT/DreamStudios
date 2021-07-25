4e93d10fa35ff5f576ad0e05a7cdb25eee259e84
MartinTorres987 <86280538+MartinTorres987@users.noreply.github.com>
2021-07-24 19:46:25 -0600

    página principal

--- Dreams!_DS Oficial/Página Principal/archivos-js/settings.js
+++ Dreams!_DS Oficial/Página Principal/archivos-js/settings.js
@@ -1,18 +1,39 @@
 const fila = document.querySelector(".contenedor-principal");
-const peliculas = document.querySelector(".artista");
+const artistas = document.querySelectorAll(".artista");
 
 const flechaIzquierda = document.getElementById("flechaIz");
 const flechaDerecha = document.getElementById("flechaDe");
+
+var long_scroll = fila.scrollLeft;
+
 
 // Event Listener para la flecha derecha
 
 flechaDerecha.addEventListener("click", () => {
     fila.scrollLeft += fila.offsetWidth;
+
+    document.querySelector(".icon-left-open-mini").classList.remove("active");
+
 })
 
 // Event Listener para la flecha Izquierda
 
 flechaIzquierda.addEventListener("click", () => {
     fila.scrollLeft -= fila.offsetWidth;
+
+
 })
 
+// ------- FILA = 0
+if((long_scroll) = 1 ){
+    document.querySelector(".icon-left-open-mini").classList.add("active");
+}
+
+
+//---------------Páginación
+
+const numpage = Math.ceil(artistas.length / 4);
+
+//for(let i = 0; i < numpage; i++){
+    //if (i === 0){
+        //document.querySelector(".icon-left-open-mini").classList.add("active");
